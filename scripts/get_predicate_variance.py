f02fcaed7a3a59fa68e7bfcbb8bf0404eb079b40
Natalie Prange <prange@informatik.uni-freiburg.de>
2024-11-21 13:59:58 +0100

    Improve computation of predicate variances
    1) Adjust QLever query to be more efficient
    2) Simplify computation of predicate variances for several base types at
       once
    3) Improve option to compute predicate variances for all third-level
       types as base types (children of children of type "entity")

--- scripts/get_predicate_variance.py
+++ scripts/get_predicate_variance.py
@@ -5,7 +5,6 @@ import subprocess
 import os
 import random
 from collections import defaultdict
-import time
 from scipy.sparse import csr_array
 
 sys.path.append(".")
@@ -72,7 +71,9 @@ class VarianceScoreComputer:
         self.debug = debug
         self.write = write
 
-        if not entity_db:
+        if entity_db:
+            self.entity_db = entity_db
+        else:
             self.entity_db = EntityDatabase()
             self.entity_db.load_reverse_subclass_of_mapping()
             self.entity_db.load_reverse_instance_of_mapping()
@@ -81,11 +82,20 @@ class VarianceScoreComputer:
             if self.debug:
                 self.entity_db.load_entity_to_name()
 
-        self.predicate_file_name = settings.DATA_DIRECTORY + f"predicates/predicates.{self.base_type}.tsv"
+        self.predicate_file_name = settings.PREDICATES_DIRECTORY + f"predicates.{self.base_type}.tsv"
+
+        # Create directory if it does not exist
+        if not os.path.exists(settings.PREDICATES_DIRECTORY):
+            os.makedirs(settings.PREDICATES_DIRECTORY)
 
         # Download the predicate file if it does not exist yet
+        self.success = True
         if not os.path.isfile(self.predicate_file_name):
-            self.download_predicate_mapping()
+            self.success = self.download_predicate_mapping()
+            if not self.success:
+                return
+        else:
+            logger.info(f"Using existing predicate file at {self.predicate_file_name}")
 
         self.predicate_index = []
         self.predicate_to_index = {}
@@ -105,10 +115,12 @@ class VarianceScoreComputer:
         predicates. Process the file by replacing URIs by entity / property IDs.
         """
         # Download the TSV file from QLever
-        logger.info(f"Download entities and their predicates for type {self.base_type} to file "
+        logger.info(f"Downloading entities and their predicates for type {self.base_type} to file "
                     f"{self.predicate_file_name} ...")
-        tmp_file = settings.DATA_DIRECTORY + "predicates/tmp_file.tsv"
+        tmp_file = settings.PREDICATES_DIRECTORY + "tmp_file.tsv"
         with open(tmp_file, 'w', encoding="utf8") as out_file:
+            # Properties appear twice once with direct (.../prop/direct/P31) and once without (.../prop/P31).
+            # I don't need both, so I use FILTER to filter them out.
             subprocess.run(['curl', '-s', 'https://qlever.cs.uni-freiburg.de/api/wikidata',
                             '-H', 'Accept: text/tab-separated-values',
                             '-H', 'Content-type: application/sparql-query',
@@ -118,31 +130,50 @@ class VarianceScoreComputer:
                             'PREFIX wd: <http://www.wikidata.org/entity/> '
                             'SELECT DISTINCT ?item ?predicate WHERE { '
                             '?item wdt:P31/wdt:P279* wd:' + self.base_type + ' . '
-                            '?item ql:has-predicate ?predicate } '],
+                            '?item ?predicate [] . '
+                            'FILTER(STRSTARTS(STR(?predicate), "http://www.wikidata.org/prop/P")) . '
+                            'FILTER(!STRSTARTS(STR(?predicate), "http://www.wikidata.org/prop/P31")) . }'
+                            ],
                            stdout=out_file)
 
-        # Simplify the format of the file by only keeping the entity/property IDs, not the whole URI
-        with open(self.predicate_file_name, "w", encoding="utf8") as out_file:
+        try:
+            # Simplify the format of the file by only keeping the entity/property IDs, not the whole URI
+            with open(self.predicate_file_name, "w", encoding="utf8") as out_file:
+                with open(tmp_file, "r", encoding="utf8") as file:
+                    for i, line in enumerate(file):
+                        # Skip first line with column headers
+                        if i == 0:
+                            continue
+                        lst = line.strip("\n").split("\t")
+                        # Skip predicates like label, description, synonyms
+                        if not lst[1].startswith("<http://www.wikidata.org/"):
+                            continue
+                        # Only keep the last element of the URI and cut off the ">" in the end
+                        for j in range(len(lst)):
+                            lst[j] = lst[j][lst[j].rfind("/") + 1:-1]
+                        out_file.write(f"{lst[0]}\t{lst[1]}\n")
+
+            # Remove duplicate lines. These occur because the same predicate can occur with a "direct"
+            # element in its path and without it.
+            with open(tmp_file, "w", encoding="utf8") as out_file:
+                sort_process = subprocess.run(['sort', self.predicate_file_name], check=True,
+                                              capture_output=True)
+                subprocess.run(["uniq"], input=sort_process.stdout, stdout=out_file)
+            subprocess.run(['mv', tmp_file, self.predicate_file_name])
+            return True
+        except IndexError:
+            # This happens when QLever returns an error, typically because it ran out of memory
+            logger.error(f"Error while processing the predicate file. QLever response was:")
             with open(tmp_file, "r", encoding="utf8") as file:
                 for i, line in enumerate(file):
-                    # Skip first line with column headers
-                    if i == 0:
-                        continue
-                    lst = line.strip("\n").split("\t")
-                    # Skip predicates like label, description, synonyms
-                    if not lst[1].startswith("<http://www.wikidata.org/"):
-                        continue
-                    # Only keep the last element of the URI and cut off the ">" in the end
-                    for j in range(len(lst)):
-                        lst[j] = lst[j][lst[j].rfind("/") + 1:-1]
-                    out_file.write(f"{lst[0]}\t{lst[1]}\n")
-
-        # Remove duplicate lines. These occur because the same predicate can occur with a "direct"
-        # element in its path and without it.
-        with open(tmp_file, "w", encoding="utf8") as out_file:
-            sort_process = subprocess.run(['sort', self.predicate_file_name], check=True, capture_output=True)
-            subprocess.run(["uniq"], input=sort_process.stdout, stdout=out_file)
-        subprocess.run(['mv', tmp_file, self.predicate_file_name])
+                    if i < 5:
+                        print(line.strip())
+                    else:
+                        break
+            # Remove the empty predicate file
+            os.remove(self.predicate_file_name)
+            logger.info(f"Predicate file removed.")
+            return False
 
     def build_indices(self):
         """
@@ -185,7 +216,7 @@ class VarianceScoreComputer:
         col_indices = []
         type_entities = self.entity_db.get_entities_for_type(type_id)
         # It can happen, that an entity is not in the entity_to_index mapping if the P31/P279 mappings
-        # are from different Wikidata versions than the predicate mapping. Therefore this check is needed.
+        # are from different Wikidata versions than the predicate mapping. Therefore, this check is needed.
         type_entities = [e for e in type_entities if e in self.entity_to_index]
         if not type_entities:
             # No entity has the given type (it is still considered a type, because it is a subclass of
@@ -219,11 +250,11 @@ class VarianceScoreComputer:
         # Get child types and parent types of the base type
         all_relevant_types = {self.base_type}
         all_relevant_types.update(self.entity_db.get_all_child_types(self.base_type))
-        all_relevant_types.update(self.entity_db.get_all_parent_types(self.base_type))
+        # all_relevant_types.update(self.entity_db.get_all_parent_types(self.base_type))
         variances = []
         num_types = len(all_relevant_types)
         for i, type_id in enumerate(all_relevant_types):
-            print(f"\rComputing variance for type {i + 1} of {num_types} ({type_id})", end="")
+            print(f"\rComputing variance for type {i + 1} of {num_types} ({type_id})       ", end="")
             variances.append((type_id, self.compute_variance_for_type(type_id)))
         print()
         logger.info("Done.")
@@ -236,7 +267,10 @@ class VarianceScoreComputer:
                     type_name = self.entity_db.get_entity_name(type_id)
                     print(f"{type_name} ({type_id}):\t{variance:.4f}")
         if self.write:
-            variance_file_name = settings.DATA_DIRECTORY + f"predicate_variances.{self.base_type}.tsv"
+            # Create directory if it does not exist
+            if not os.path.exists(settings.PREDICATE_VARIANCES_DIRECTORY):
+                os.makedirs(settings.PREDICATE_VARIANCES_DIRECTORY)
+            variance_file_name = settings.PREDICATE_VARIANCES_DIRECTORY + f"predicate_variances.{self.base_type}.tsv"
             logger.info(f"Writing variances to {variance_file_name} ...")
             with open(variance_file_name, "w", encoding="utf8") as out_file:
                 for type_id, variance in variances:
@@ -276,7 +310,10 @@ class VarianceScoreComputer:
 
 
 def main(args):
-    if args.type_qid == "all":
+    error_qids = []
+    no_predicates_qids = []
+    success_qids = []
+    if "all" in args.type_qid:
         entity_db = EntityDatabase()
         entity_db.load_reverse_subclass_of_mapping()
         entity_db.load_reverse_instance_of_mapping()
@@ -290,36 +327,68 @@ def main(args):
         score_file_name = settings.DATA_DIRECTORY + f"predicate_variance_scores.all.tsv"
         variance_out_file = open(variance_file_name, "w", encoding="utf8")
         score_out_file = open(score_file_name, "w", encoding="utf8")
-        for base_type in third_order_types:
-            variance_computer = VarianceScoreComputer(base_type, False, entity_db, write=False)
-            variances = variance_computer.compute_variances()
-            for type_id, variance in variances:
-                variance_out_file.write(f"{type_id}\t{variance:.4f}\n")
-            scores = variance_computer.compute_variance_scores(variances)
-            for type_id, score in scores:
-                score_out_file.write(f"{type_id}\t{score:.4f}\n")
+        logger.info(f"Computing variances for {len(third_order_types)} types ...")
+        # logger.info(f"Selected types: {', '.join(third_order_types)}")
+        for i, base_type in enumerate(third_order_types):
+            variance_computer = VarianceScoreComputer(base_type, args.debug, entity_db=entity_db, write=not args.no_write)
+            if not variance_computer.success:
+                error_qids.append(base_type)
+                continue
+            if len(variance_computer.predicate_index) == 0:
+                logger.info(f"Skipping type {base_type} with no entity predicates.")
+                no_predicates_qids.append(base_type)
+                continue
+            variance_computer.compute_variances()
+            success_qids.append(base_type)
+            logger.info(f"DONE computing variance for type {i+1} out of {len(third_order_types)}")
 
+            # for type_id, variance in variances:
+            #     variance_out_file.write(f"{type_id}\t{variance:.4f}\n")
+            # scores = variance_computer.compute_variance_scores(variances)
+            # for type_id, score in scores:
+            #     score_out_file.write(f"{type_id}\t{score:.4f}\n")
     else:
-        variance_computer = VarianceScoreComputer(args.type_qid, args.debug, write=not args.no_write)
-        variances = variance_computer.compute_variances()
+        entity_db = EntityDatabase()
+        entity_db.load_reverse_subclass_of_mapping()
+        entity_db.load_reverse_instance_of_mapping()
+        entity_db.load_subclass_of_mapping()
 
-        if args.generate_plot:
-            variance_computer.compute_variance_scores(variances)
-            plot_variances(variances, args.type_qid)
-        print(variance_computer.timings)
+        if args.debug:
+            entity_db.load_entity_to_name()
 
+        for qid in args.type_qid:
+            variance_computer = VarianceScoreComputer(qid, args.debug, entity_db=entity_db, write=not args.no_write)
+            if not variance_computer.success:
+                error_qids.append(qid)
+                continue
+            if len(variance_computer.predicate_index) == 0:
+                logger.info(f"Skipping type {qid} with no entity predicates.")
+                no_predicates_qids.append(qid)
+                continue
+            variances = variance_computer.compute_variances()
+            success_qids.append(qid)
+
+            if args.generate_plot:
+                variance_computer.compute_variance_scores(variances)
+                plot_variances(variances, args.type_qid)
+
+    logger.info(f"Successfully computed variances for {len(success_qids)} QIDs: {', '.join(success_qids)}")
+    if no_predicates_qids:
+        logger.info(f"Skipped {len(no_predicates_qids)} QIDs with no entity predicates: {', '.join(no_predicates_qids)}")
+    if error_qids:
+        logger.info(f"Could not compute variances for {len(error_qids)} QIDs: {', '.join(error_qids)}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description=__doc__)
 
-    parser.add_argument("-qid", "--type_qid", type=str, required=True,
+    parser.add_argument("-qid", "--type_qid", type=str, required=True, nargs="+",
                         help="QID of the type for which to fetch all entities with their predicates.")
     parser.add_argument("-d", "--debug", action="store_true",
                         help="Print debug information.")
     parser.add_argument("-p", "--generate_plot", action="store_true",
                         help="Generate a plot of the predicate variances.")
-    parser.add_argument("-w", "--no_write", action="store_true",
+    parser.add_argument("-nw", "--no_write", action="store_true",
                         help="Don't write the computed variances to file.")
 
     logger = log.setup_logger()
